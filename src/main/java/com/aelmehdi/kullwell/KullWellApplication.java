d673987d82cd4a040d810ca6ed69608772e3d146
AElMehdi <assirelmehdi@gmail.com>
2020-05-29 23:37:47 +0200

    Init spring boot

---
+++ src/main/java/com/aelmehdi/kullwell/KullWellApplication.java
@@ -0,0 +1,13 @@
+package com.aelmehdi.kullwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KullWellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KullWellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aelmehdi/kullwell/KullWellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aelmehdi.kullwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KullWellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
