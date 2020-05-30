0d34802df02e37d1b5ba804f997318f76dd49f61
AElMehdi <assirelmehdi@gmail.com>
2020-05-30 10:52:44 +0200

    Test repository layer using testcontainers

---
+++ src/test/java/com/aelmehdi/kullwell/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.aelmehdi.kullwell.repositories;
+
+import com.aelmehdi.kullwell.models.Customer;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository userRepository;
+
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:latest");
+
+    @Test
+    void should_save_a_user() {
+        Customer savedUser = userRepository.save(new Customer(1L, "Yassine", "Nissya"));
+
+        assertThat(savedUser.getFirstName()).isEqualTo("Yassine");
+    }
+}
